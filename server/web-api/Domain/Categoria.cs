5afdbd0f8827996e7f3cad2cfa2b359b746abab0
gkirschnxr <kirschnxr@gmail.com>
2025-11-13 13:45:54 -0300

    adicionado projeto backend

---
+++ server/web-api/Domain/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace NoteKeeper.WebApi.Domain;
+
+public class Categoria : Entidade
+{
+    public string Titulo { get; set; }
+    public List<Nota> Notas { get; set; } = [];
+
+    public Categoria(string titulo) {
+        Titulo = titulo;
+    }
+}
---
+++ server/web-api/Orm/MapeadorCategoriaOrm.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NoteKeeper.WebApi.Domain;
+
+namespace NoteKeeper.WebApi.Orm;
+
+public class MapeadorCategoriaOrm : IEntityTypeConfiguration<Categoria>
+{
+    public void Configure(EntityTypeBuilder<Categoria> builder) {
+        builder.ToTable("TBCategoria");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Titulo)
+            .IsRequired();
+    }
+}
